7c045539ad101bd865e4c206a638d7f866b05209
tumwinep <tumwinep@gmail.com>
2020-09-24 22:15:19 +0300

    Added Logout menu on nav bar
    Added Logout menu on nav bar

---
+++ SoftDevCase/UserLogOut.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="UserLogOut.aspx.cs" Inherits="SoftDevCase.UserLogOut" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="SiteMainContent" runat="server">
+
+</asp:Content>
---
+++ SoftDevCase/UserLogOut.aspx.cs
@@ -0,0 +1,24 @@
+﻿using SoftDevCase.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace SoftDevCase
+{
+    public partial class UserLogOut : System.Web.UI.Page
+    {
+        BusinessLogic bl = new BusinessLogic();
+        Encryption enc = new Encryption();
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SessionManager sh = new SessionManager();
+            sh.destroySession();
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
